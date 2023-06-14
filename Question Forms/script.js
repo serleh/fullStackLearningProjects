082c2766019fdb18075e130c98b9a519da00ced7
Saleh Abdullahi <salehabdullahi018@gmail.com>
2023-06-14 14:47:48 +0100

    tried making the checkbox change to radio, but it did'nt work

--- Question Forms/script.js
+++ Question Forms/script.js
@@ -6,7 +6,7 @@ const data = [
             '828m', '231m', '782m', '926m'
         ],
         key: '828m',
-        isMulti: true,
+        isMulti: false,
     },
     {
         question: 'What is the diameter of Burj',
@@ -60,6 +60,9 @@ function addOptions(){
     const questionHeading = bodyNode.querySelector('#questionHeading');
     const question = bodyNode.querySelector('#question');
     const options = bodyNode.querySelector('#options');
+    let input = document.querySelector('input')
+    
+    console.log(input)
 
     questionHeading.innerText = 'Question 1';
     question.innerText = data[0].question;
@@ -67,7 +70,16 @@ function addOptions(){
     for (let i = 0; i < options.children.length; i++) {
         const element = options.children[i];
         element.querySelector('label').innerText = data[0].options[i]
+
+        // tried making the checkbox change to radio, but it did'nt work
+         if(data.isMulti === false){
+          input.type = 'radio'
+       
     }
+}
+
+  
+   
 };
 
 
